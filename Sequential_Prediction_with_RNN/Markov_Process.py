90df9b9a14df9960beb850605e83ceae3764462c
omerlux <omerlux1994@gmail.com>
2020-08-05 18:04:29 +0300

    Markov model - created RNN model which predicts the *transition matrix*.

--- Sequential_Prediction_with_RNN/Markov_Process.py
+++ Sequential_Prediction_with_RNN/Markov_Process.py
@@ -69,69 +69,73 @@ for i in range(N):
 #     'C' = 1.25/7.25 = ~172
 # Which is as the @stationary_distribution in Section (b).
 print("The stationary distribution is:\n" +
-        "\tA = " + '%.5f' % (5/7.25) + "\n"
-        "\tB = " + '%.5f' % (1/7.25) + "\n"
-        "\tC = " + '%.5f' % (1.25/7.25) + "\n")
+      "\tA = " + '%.5f' % (5 / 7.25) + "\n"
+                                       "\tB = " + '%.5f' % (1 / 7.25) + "\n"
+                                                                        "\tC = " + '%.5f' % (1.25 / 7.25) + "\n")
 
 # Section(b) - Generating the stationary distribution:
 unique, counts = np.unique(samples, return_counts=True)
 counts = counts / (N + 1)
 stationary_distribution = dict(zip(unique, counts))
 print("The approximately stationary distribution is:\n" +
-        "\tA = " + '%.5f' % stationary_distribution['A'] + "\n"
-        "\tB = " + '%.5f' % stationary_distribution['B'] + "\n"
-        "\tC = " + '%.5f' % stationary_distribution['B'] + "\n")
+      "\tA = " + '%.5f' % stationary_distribution['A'] + "\n"
+                                                         "\tB = " + '%.5f' % stationary_distribution['B'] + "\n"
+                                                                                                            "\tC = " + '%.5f' %
+      stationary_distribution['B'] + "\n")
 
 # Section(c) -
 # Generating train and test:
 tests = [random.choice(['A', 'B', 'C'])]
 for i in range(N):
-    tests.append(markov_next(samples[i]))
+    tests.append(markov_next(tests[i]))
 y_train = markov_converter(samples)
 x_train = []
 y_test = markov_converter(tests)
 x_test = []
-for i in range(1, N + 1):
-    x_train.append(np.argmax([y_train[i - 1]], axis=1))
-    x_test.append(np.argmax([y_test[i - 1]], axis=1))
-x_train = np.array(x_train).reshape(-1, 1, 1)
-y_train = np.array(y_train[1:N + 1]).reshape(-1, 1, 3)
-x_test = np.array(x_test).reshape(-1, 1, 1)
-y_test = np.array(x_test[1:N + 1]).reshape(-1, 1, 3)
+for i in range(3, N + 1):
+    x_train.append(np.argmax([y_train[i - 1], y_train[i - 2], y_train[i - 3]], axis=1))
+    x_test.append(np.argmax([y_test[i - 1], y_test[i - 2], y_test[i - 3]], axis=1))
+x_train = (np.array(x_train) / 2.0).reshape(-1, 3, 1)
+y_train = np.array(y_train[3:N + 1]).reshape(-1, 3, )
+x_test = (np.array(x_test) / 2.0).reshape(-1, 3, 1)
+y_test = np.array(y_test[3:N + 1]).reshape(-1, 3, )
 
 # 50 samples of the process:
 x_axis = np.transpose(np.array(range(50)))
-plot(x_axis, np.argmax(y_train[:50], axis=2).reshape(1, -1), "Markov process of 50 samples")
+plot(x_axis, np.argmax(y_train[:50], axis=1).reshape(1, -1), "Markov process of 50 samples")
 
 # --------------------MODEL - COMPILE & FIT--------------------
 
 # Creating RNN model and fit it:
 model_RNN = keras.Sequential()
-# Add a LSTM layer with 64 internal units.
-model_RNN.add(layers.LSTM(32, input_shape=(1, 1), return_sequences=True))  # time steps = 3, dim = 1
-# Add a LSTM layer with 64 internal units.
-model_RNN.add(layers.LSTM(16, return_sequences=False))
+# Add a LSTM layer with 32 internal units.
+model_RNN.add(layers.LSTM(32, input_shape=(3, 1), return_sequences=False))  # time steps = 3, dim = 1
 # Add a Dense layer with 1 units.
-model_RNN.add(layers.Dense(3))
+model_RNN.add(layers.Dense(3, activation='softmax'))
 model_RNN.summary()
 # keras.utils.plot_model(model_RNN, "imgs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+'_Markov_Model_RNN.png', show_shapes=True) # Model scheme
 
 model_RNN.compile(
-    loss='binary_crossentropy',
+    loss='categorical_crossentropy',
     optimizer="RMSprop",
     metrics=['accuracy'],
 )
 # log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "_Markov_Model_RNN"
 # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 model_RNN.fit(
-    x_train, y_train, batch_size=32, epochs=10, verbose=2  # , callbacks=[tensorboard_callback]
+    x_train, y_train, batch_size=32, epochs=3, verbose=2  # , callbacks=[tensorboard_callback]
 )
 
 # Prediction:
 out_vec = model_RNN.predict(x_test)
-out_as_dist = sum(out_vec) / len(out_vec)
-model_distribution = dict(zip(['A', 'B', 'C'], out_as_dist))
-print("The model distribution is:\n" +
-        "\tA = " + '%.5f' % out_as_dist[0] + "\n"
-        "\tB = " + '%.5f' % out_as_dist[1] + "\n"
-        "\tC = " + '%.5f' % out_as_dist[2] + "\n")
+PA = out_vec[np.squeeze([x[0] == 0 for x in x_test])]
+PB = out_vec[np.squeeze([x[0] == 0.5 for x in x_test])]
+PC = out_vec[np.squeeze([x[0] == 1 for x in x_test])]
+PA = np.transpose(sum(PA) / len(PA))
+PB = np.transpose(sum(PB) / len(PB))
+PC = np.transpose(sum(PC) / len(PC))
+print("The model transition matrix is:")
+np.set_printoptions(suppress=True, precision=5)
+print("P(x|A) -> ", end=""), print(PA)
+print("P(x|B) -> ", end=""), print(PB)
+print("P(x|C) -> ", end=""), print(PC)
