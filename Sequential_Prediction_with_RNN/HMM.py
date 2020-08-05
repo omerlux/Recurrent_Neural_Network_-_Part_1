7654bfbd4e9bafa4f3729bd212ef61aaa692b309
omerlux <omerlux1994@gmail.com>
2020-08-05 20:25:08 +0300

    Hidden Markov Model - given only 0 and 1 outputs, the network can predict the hidden 3 state markov model.

--- Sequential_Prediction_with_RNN/HMM.py
+++ Sequential_Prediction_with_RNN/HMM.py
@@ -12,16 +12,16 @@ from tensorflow.keras import layers
 
 
 def plot(x, y, title, legends=None):  # Legends should be list. Title is string
-    plt.title("Markov Model - " + title)
+    plt.title("HMM - " + title)
     for j in range(len(y)):
         if j == 1:
             plt.plot(x, y[j], linestyle=':', marker='.')
         else:
             plt.plot(x, y[j], linestyle='--', marker='o')
     if legends is not None:
-        plt.legend(legends, loc='upper center', bbox_to_anchor=(0.5, -0.1))
+        plt.legend(legends)
     plt.grid(True)
-    # plt.savefig("imgs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "_Markov_model-" + title)
+    # plt.savefig("imgs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "_HMM-" + title)
     plt.show()
 
 
@@ -42,13 +42,7 @@ def markov_next(prev):
 
 def markov_converter(markov_process):
     """@:returns a markov process of numbers - not letters"""
-    return [[1, 0, 0] if x == 'A' else ([0, 1, 0] if x == 'B' else [0, 0, 1]) for x in markov_process]
-
-
-def markov_inverter(markov_process):
-    """@:returns a markov process of numbers - not letters"""
-    # TODO: Maybe change to 0, 1, 2
-    return ['A' if x == 0 else ('B' if x == 1 else 'C') for x in markov_process]
+    return [[0, 1] if x == 'A' else ([1, 0] if x == 'B' else [1, 0]) for x in markov_process]
 
 
 # Defining the process:
@@ -58,26 +52,13 @@ for i in range(N):
     samples.append(markov_next(samples[i]))
 
 # Section (a):
-# By calculating analytically:
-#     'A' = 5/7.25 = ~0.689
-#     'B' = 1/7.25 = ~0.137
-#     'C' = 1.25/7.25 = ~172
-# Which is as the @stationary_distribution in Section (b).
+# This is a Hidden Markov model - we only observe '0' or '1' but obliviously there are 3 different states
 print("The stationary distribution is:\n" +
-      "\tA = " + '%.5f' % (5 / 7.25) + "\n"
-                                       "\tB = " + '%.5f' % (1 / 7.25) + "\n"
-                                                                        "\tC = " + '%.5f' % (1.25 / 7.25) + "\n")
+       "\tA = " + '%.5f' % (5 / 7.25) + "\n"
+       "\tB = " + '%.5f' % (1 / 7.25) + "\n"
+       "\tC = " + '%.5f' % (1.25 / 7.25) + "\n")
 
-# Section(b) - Generating the stationary distribution:
-unique, counts = np.unique(samples, return_counts=True)
-counts = counts / (N + 1)
-stationary_distribution = dict(zip(unique, counts))
-print("The approximately stationary distribution is:\n" +
-      "\tA = " + '%.5f' % stationary_distribution['A'] + "\n"
-      "\tB = " + '%.5f' % stationary_distribution['B'] + "\n"
-      "\tC = " + '%.5f' % stationary_distribution['B'] + "\n")
-
-# Section(c) -
+# Section(b) -
 # Generating train and test:
 tests = [random.choice(['A', 'B', 'C'])]
 for i in range(N):
@@ -89,14 +70,14 @@ x_test = []
 for i in range(3, N + 1):
     x_train.append(np.argmax([y_train[i - 1], y_train[i - 2], y_train[i - 3]], axis=1))
     x_test.append(np.argmax([y_test[i - 1], y_test[i - 2], y_test[i - 3]], axis=1))
-x_train = (np.array(x_train) / 2.0).reshape(-1, 3, 1)
-y_train = np.array(y_train[3:N + 1]).reshape(-1, 3, )
-x_test = (np.array(x_test) / 2.0).reshape(-1, 3, 1)
-y_test = np.array(y_test[3:N + 1]).reshape(-1, 3, )
+x_train = (np.array(x_train)).reshape(-1, 3, 1)
+y_train = np.array(y_train[3:N + 1]).reshape(-1, 2, )   # y will be only 0 or 1...
+x_test = (np.array(x_test)).reshape(-1, 3, 1)
+y_test = np.array(y_test[3:N + 1]).reshape(-1, 2, )     # y will be only 0 or 1...
 
 # 50 samples of the process:
 x_axis = np.transpose(np.array(range(50)))
-plot(x_axis, np.argmax(y_train[:50], axis=1).reshape(1, -1), "Markov process of 50 samples", ["A=0, B=0.5, C=1"])
+plot(x_axis, np.argmax(y_train[:50], axis=1).reshape(1, -1), "Markov process of 50 samples")
 
 # --------------------MODEL - COMPILE & FIT--------------------
 
@@ -104,9 +85,8 @@ plot(x_axis, np.argmax(y_train[:50], axis=1).reshape(1, -1), "Markov process of 
 model_RNN = keras.Sequential()
 # Add a LSTM layer with 32 internal units.
 model_RNN.add(layers.LSTM(32, input_shape=(3, 1), return_sequences=False))  # time steps = 3, dim = 1
-""" NOTE: We can change the input time step to 1, because of markovity, and get the same result."""
-# Add a Dense layer with 3 units - output is 0 1 or 2 (as A B or C)
-model_RNN.add(layers.Dense(3, activation='softmax'))
+# Add a Dense layer with 2 units - output is only 1 or 0 (as A or B/C)
+model_RNN.add(layers.Dense(2, activation='softmax'))
 model_RNN.summary()
 # keras.utils.plot_model(model_RNN, "imgs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+'_Markov_Model_RNN.png', show_shapes=True) # Model scheme
 
@@ -122,15 +102,25 @@ model_RNN.fit(
 )
 
 # Prediction:
+# We will check out all the different 2 consecutive numbers' transition probability of the process,
+# if we'll get the same transitions as the HMM transition matrix, the network predicted the hidden model perfectly
 out_vec = model_RNN.predict(x_test)
-PA = out_vec[np.squeeze([x[0] == 0 for x in x_test])]
-PB = out_vec[np.squeeze([x[0] == 0.5 for x in x_test])]
-PC = out_vec[np.squeeze([x[0] == 1 for x in x_test])]
-PA = np.transpose(sum(PA) / len(PA))
-PB = np.transpose(sum(PB) / len(PB))
-PC = np.transpose(sum(PC) / len(PC))
+# P (t-1), (t-2)
+P11 = out_vec[np.squeeze([(x[0] == 1 and x[1] == 1) for x in x_test])]
+P10 = out_vec[np.squeeze([(x[0] == 1 and x[1] == 0) for x in x_test])]
+P01 = out_vec[np.squeeze([(x[0] == 0 and x[1] == 1) for x in x_test])]
+P00 = out_vec[np.squeeze([(x[0] == 0 and x[1] == 0) for x in x_test])]
+P11 = np.transpose(sum(P11) / len(P11))
+P10 = np.transpose(sum(P10) / len(P10))
+P01 = np.transpose(sum(P01) / len(P01))
+P00 = np.transpose(sum(P00) / len(P00))
+
+
 print("The model transition matrix is:")
+print("Trnasition | out=0 | out =1 |")
+print("-----------+-------+--------+")
 np.set_printoptions(suppress=True, precision=5)
-print("P(x|A) -> ", end=""), print(PA)
-print("P(x|B) -> ", end=""), print(PB)
-print("P(x|C) -> ", end=""), print(PC)
+print("P(x|1←1):  ", end=""), print(P11),
+print("P(x|1←0):  ", end=""), print(P10)
+print("P(x|0←1):  ", end=""), print(P01)
+print("P(x|0←0):  ", end=""), print(P00)
